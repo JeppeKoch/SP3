c217cf813336b0e86cc8c6260fb446df6dcbc990
MINGO-INC <143798588+MINGO-INC@users.noreply.github.com>
2023-11-20 11:51:45 +0100

    done

---
+++ SP3_Domain/src/Media.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class Media {
+
+private String title;
+private ArrayList<String> genres=new ArrayList<>();
+private String releasDate;
+private double rating;
+    Media(String title, String releasDate,ArrayList<String> genres,double rating){
+this.title=title;
+this.releasDate=releasDate;
+this.genres=genres;
+this.rating=rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String toString() {
+        return "this movie{" +
+                "title='" + title + '\'' +
+                ", genres=" + genres +
+                ", releasDate='" + releasDate + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+
+    public String showCategories(){
+        String categories="";
+        for (String c: genres){
+            categories+=c+",";
+        }
+        return categories;
+
+
+    }
+}
