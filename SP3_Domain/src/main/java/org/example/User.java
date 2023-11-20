8f52b1257ec9a8321b1dd0c8a779670cc1b45eef
JeppeKoch <cph-jk469@cphbusiness.dk>
2023-11-20 09:35:12 +0100

    d

--- SP3_Domain/src/main/java/org/example/User.java
+++ SP3_Domain/src/main/java/org/example/User.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class User {
+private String name;
+private String username;
+private  String password;
+//  recentlyViewed
+    ArrayList <Media> watchList = new ArrayList<>();
+    ArrayList <Media> saveMedia = new ArrayList<>();
+    ArrayList <Media> allMedia = new ArrayList<>();
+
+    public void addMedia(){
+
+
+    }
+    
+    public void removeMedia(){
+
+
+    }
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName(){
+        return name;
+
+    }
+
+    public String getUsername(){
+        return username;
+
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
