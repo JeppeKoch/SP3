612acd5704e9e96905413ccb79ab41bfcb52a7ea
JeppeKoch <cph-jk469@cphbusiness.dk>
2023-11-20 11:05:23 +0100

    a

--- SP3_Domain/src/main/java/org/example/User.java
+++ SP3_Domain/src/main/java/org/example/User.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 
 public class User {
 private String name;
-private String username;
-private  String password;
 
 //  recentlyViewed laves senere
-    ArrayList <Media> watchList = new ArrayList<>();
-    ArrayList <Media> saveMedia = new ArrayList<>();
-    ArrayList <Media> allMedia = new ArrayList<>();
+    ArrayList <Media> watchList;
+    ArrayList <Media> saveMedia;
+    ArrayList <Media> allMedia;
 
-    public void addMedia(){
-
-
+    public void addMedia(Media media){
+    watchList.add(media);
+    saveMedia.add(media);
     }
 
-    public void removeMedia(){
-
-
+    public void removeMedia(Media media){
+    watchList.add(media);
+    saveMedia.add(media);
     }
 
-    public User(String name, ) {
+    public User(String name) {
         this.name = name;
+        this.watchList = new ArrayList<>();
+        this.saveMedia = new ArrayList<>();
+        this.allMedia = new ArrayList<>();
 
     }
 
